ba01fe78432b2facf9456ba97519b530cfb760da
aleksolh <aleksolh@stud.ntnu.no>
2023-04-06 23:54:48 +0200

    Now also draws the stone on the board

--- src/DrawFunctions.hs
+++ src/DrawFunctions.hs
@@ -30,3 +30,27 @@ drawBoard renderer board boardSize = do
                       Just Black -> drawStone renderer Black (x, y)       -- Draws the black stone at the coordinate
                       Just White -> drawStone renderer White (x, y)       -- draws the white stone at the coordinate
                       Nothing -> return ()) [(fromIntegral x, fromIntegral y) | x <- [0..boardSize-1], y <- [0..boardSize-1]]
+
+-- | Draws the stone at the given coordinate by finding the center of the square and then calculating the radius so that the circle
+-- is within the square (Not really a circle more like a rotated square)
+drawStone :: SDL.Renderer -> Stone -> Coordinate -> IO ()
+drawStone renderer stone (x, y) = do
+  let color = case stone of               -- Pattern matching stone to use the correct color
+                Black -> V4 0 0 0 255
+                White -> V4 255 255 255 255
+      -- sclaes the coordinate by the tilesize and then adds half the tilesize so it is in the center
+      center = SDL.P (SDL.V2 (fromIntegral x * tileSize + tileSize `div` 2) (fromIntegral y * tileSize + tileSize `div` 2))
+      radius = tileSize `div` 2 - 4                  -- -4 so it does not reach all the edges of the square, looks a bit better
+  SDL.rendererDrawColor renderer SDL.$= color
+  fillCircle renderer center radius             -- Fills the circle with the specified color
+
+-- | This function fills the circle with the specified color. It uses a circle equation to only draw the points that are within
+-- the circle. The nested forM_ functions iterate over all the points within a square that is found using the center points of the circle
+-- and the radius of the circle. Then for every point within that square it checks if the point is within the circle calculated using the
+--  circle equation.
+fillCircle :: SDL.Renderer -> SDL.Point SDL.V2 CInt -> CInt -> IO ()
+fillCircle renderer (SDL.P (SDL.V2 cx cy)) radius = do
+  let circleCheck x y = (x - cx) ^ 2 + (y - cy) ^ 2 <= radius ^ 2
+  forM_ [cy - radius .. cy + radius] $ \y ->
+    forM_ [cx - radius .. cx + radius] $ \x -> do
+      when (circleCheck x y) $ SDL.drawPoint renderer (SDL.P (SDL.V2 x y))
